3e021a538e228b4faef0afb0c26c8709c826dab6
Bruno Farias <contato.brunofarias@gmail.com>
2019-12-12 22:16:53 -0300

    Stetho

--- app/src/main/java/com/example/helloworldwebservice/MainActivity.kt
+++ app/src/main/java/com/example/helloworldwebservice/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.helloworldwebservice
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.facebook.stetho.okhttp3.StethoInterceptor
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -24,9 +26,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun pesquisar() {
+        val okhttpClient = OkHttpClient.Builder()
+            .addNetworkInterceptor(StethoInterceptor())
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl("https://api.github.com")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okhttpClient)
             .build()
 
         val gitHubService = retrofit.create(GitHubService::class.java)
@@ -47,6 +54,8 @@ class MainActivity : AppCompatActivity() {
                         tvUsuario.text = usuario?.nome
                         Picasso.get()
                             .load(usuario?.avatarUrl)
+                            .error(R.mipmap.ic_launcher)
+                            .placeholder(R.mipmap.ic_launcher)
                             .into(ivUsuario)
                     } else {
                         Toast.makeText(
--- app/src/main/java/com/example/helloworldwebservice/MyApplication.kt
+++ app/src/main/java/com/example/helloworldwebservice/MyApplication.kt
@@ -1,2 +1,13 @@
 package com.example.helloworldwebservice
 
+import android.app.Application
+import com.facebook.stetho.Stetho
+
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG)
+            Stetho.initializeWithDefaults(this)
+    }
+}
